715c6cf51ce112662c9fe7794d2caaea6f563766
Do Hoerin <hellodhlyn@gmail.com>
2020-05-17 22:36:01 +0900

    Removing nodejs stuffs

--- types/mocha-prepare/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "mocha-prepare" {
-  export default function prepare(
-    onPrepare: (done: (e?: any) => void) => void,
-    onUnprepare?: (done: (e?: any) => void) => void,
-  ): void;
-}
